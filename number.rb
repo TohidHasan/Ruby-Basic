c2671bf6ca3bbf0f0357bae1daedcb8af78bc7d6
Tohid <tohid2295@gmail.com>
2018-12-04 02:06:57 +0600

    Add mul sub division

---
+++ number.rb
@@ -0,0 +1,13 @@
+
+puts "Enter your First Number"
+
+firstNunber = gets.chomp
+
+
+puts "Enter your Second Number"
+
+secondNumber = gets.chomp
+puts "Addition #{firstNunber.to_i + secondNumber.to_i}"
+puts "Subtraction #{firstNunber.to_i - secondNumber.to_i}"
+puts "Multiplication #{firstNunber.to_i * secondNumber.to_i}"
+puts "Division #{firstNunber.to_i / secondNumber.to_i}"
